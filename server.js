600f5fb966ae0673b5944609dea3410af9494638
orcuntuna <turboindirblog@gmail.com>
2020-04-20 12:00:23 +0300

    install and use middlewares

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 const express = require('express')
+const morgan = require('morgan')
+const xss_clean = require('xss-clean')
+const helmet = require('helmet')
+const cors = require('cors')
+const hpp = require('hpp')
 require('dotenv').config()
 const database = require('./helpers/database')
 
@@ -6,6 +11,17 @@ const app = express()
 
 // body parser
 app.use(express.json())
+
+// dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
+// security middlewares
+app.use(helmet())
+app.use(xss_clean())
+app.use(hpp())
+app.use(cors())
 
 // connect database
 database.connect()
